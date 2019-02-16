446cc13cbb284864d7a33081989221ffa386d491
Sonia Mathew <sonia@lawn-143-215-60-40.lawn.gatech.edu>
2019-02-16 18:56:24 -0500

    react-app-template

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+//by convention the class name is same as the file name
+class App extends Component {
+    // Component class already takes care of attaching the render method to 'this' object
+    render(){
+        return (
+            <div>
+                <h1>Create React App</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
+
